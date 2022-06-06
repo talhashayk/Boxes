84dcbfebb585764e4ad8d941754ff3bd5184f0ad
Talha Sheikh <47197243+talhashayk@users.noreply.github.com>
2022-06-06 20:53:04 +0100

    Add files via upload

---
+++ src/boxes.js
@@ -0,0 +1,163 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+	{
+		id: 1,
+		on: true,
+	},
+	{
+		id: 2,
+		on: false,
+	},
+	{
+		id: 3,
+		on: true,
+	},
+	{
+		id: 4,
+		on: true,
+	},
+	{
+		id: 5,
+		on: false,
+	},
+	{
+		id: 6,
+		on: false,
+	},
+	{
+		id: 7,
+		on: false,
+	},
+	{
+		id: 8,
+		on: true,
+	},
+	{
+		id: 9,
+		on: true,
+	},
+	{
+		id: 10,
+		on: false,
+	},
+	{
+		id: 11,
+		on: true,
+	},
+	{
+		id: 12,
+		on: false,
+	},
+	{
+		id: 13,
+		on: true,
+	},
+	{
+		id: 14,
+		on: true,
+	},
+	{
+		id: 15,
+		on: false,
+	},
+	{
+		id: 16,
+		on: false,
+	},
+	{
+		id: 17,
+		on: false,
+	},
+	{
+		id: 18,
+		on: true,
+	},
+	{
+		id: 19,
+		on: true,
+	},
+	{
+		id: 20,
+		on: false,
+	},
+	{
+		id: 21,
+		on: true,
+	},
+	{
+		id: 22,
+		on: false,
+	},
+	{
+		id: 23,
+		on: true,
+	},
+	{
+		id: 24,
+		on: true,
+	},
+	{
+		id: 25,
+		on: false,
+	},
+	{
+		id: 26,
+		on: false,
+	},
+	{
+		id: 27,
+		on: false,
+	},
+	{
+		id: 28,
+		on: true,
+	},
+	{
+		id: 29,
+		on: true,
+	},
+	{
+		id: 30,
+		on: false,
+	},
+	{
+		id: 31,
+		on: true,
+	},
+	{
+		id: 32,
+		on: false,
+	},
+	{
+		id: 33,
+		on: true,
+	},
+	{
+		id: 34,
+		on: true,
+	},
+	{
+		id: 35,
+		on: false,
+	},
+	{
+		id: 36,
+		on: false,
+	},
+	{
+		id: 37,
+		on: false,
+	},
+	{
+		id: 38,
+		on: true,
+	},
+	{
+		id: 39,
+		on: true,
+	},
+	{
+		id: 40,
+		on: false,
+	},
+];
